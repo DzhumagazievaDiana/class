9d58fd8406521b07e63a965892d623b8f307082c
DzhumagazievaDiana <113889448+DzhumagazievaDiana@users.noreply.github.com>
2023-03-27 10:23:31 +0300

    Add files via upload

---
+++ Задание2/Clothes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание2
+{
+    internal class Clothes
+    {
+        private string brand;
+        private string style;
+        private int price;
+
+        public string Brand
+        {
+            get { return brand; }   
+            set { brand = value; }
+        }
+        public string Style
+        {
+            get { return style; }   
+            set { style = value; }
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+        public Clothes()
+        {
+            brand = "";
+            style = "";
+            price = 0;
+        }
+      
+        public Clothes(string brand, string style, int price)
+        {
+            this.brand = brand;
+            this.style = style;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+            return "Бренд: " + brand + ",Стиль: " + style + ", Цена: " + price;
+        }
+
+
+    }
+}
---
+++ Задание2/Program.cs
@@ -0,0 +1,13 @@
+﻿using Задание2;
+
+Clothes[] clothesArray = new Clothes[5];
+clothesArray[0] = new Clothes("Gucci", "Классический", 32000);
+clothesArray[1] = new Clothes("Chanel", "Повседневный", 10000);
+clothesArray[2] = new Clothes("Balenciaga", "Минимализм", 12000);
+clothesArray[3] = new Clothes("Fendi", "Этно", 7000);
+clothesArray[4] = new Clothes("Dior", "Гламур", 5000);
+
+foreach(Clothes clothes in clothesArray)
+{
+    Console.WriteLine(clothes);
+}
